fd02292eb301631b4cefd2fdacaa8ac3ac4da954
Max <kavun.onficial@gmail.com>
2025-02-19 19:39:46 +0100

    feat: implemented PlayerPersistenceException for better exception handling in DAO layer

---
+++ src/main/java/com/maxkavun/exception/PlayerPersistenceException.java
@@ -0,0 +1,8 @@
+package com.maxkavun.exception;
+
+public class PlayerPersistenceException extends RuntimeException {
+
+    public PlayerPersistenceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
