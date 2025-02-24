2a3ba7c7e6ec94cd73d8a4f2fadf6564fb72449c
Max <kavun.onficial@gmail.com>
2025-02-24 15:39:53 +0100

    feat: added an overloaded constructor

--- src/main/java/com/maxkavun/exception/PlayerPersistenceException.java
+++ src/main/java/com/maxkavun/exception/PlayerPersistenceException.java
@@ -5,4 +5,9 @@ public class PlayerPersistenceException extends RuntimeException {
     public PlayerPersistenceException(String message, Throwable cause) {
         super(message, cause);
     }
+
+    public PlayerPersistenceException(String message) {
+        super(message);
+    }
 }
+
