8c7bfeda10cb48ecc0d16ea50b6d58c44606cef5
Max <kavun.onficial@gmail.com>
2025-02-25 15:44:06 +0100

    refactor: refactor NewPlayersService to handle exceptions and add error logging

--- src/main/java/com/maxkavun/service/NewPlayersService.java
+++ src/main/java/com/maxkavun/service/NewPlayersService.java
@@ -1,6 +1,8 @@
 package com.maxkavun.service;
 
 import com.maxkavun.dao.PlayerDao;
+import com.maxkavun.exception.PlayerPersistenceException;
+import com.maxkavun.exception.PlayerServiceException;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -10,9 +12,13 @@ public class NewPlayersService {
     private final PlayerDao playerDao = new PlayerDao();
 
     public void createPlayersIfNotExists(String player1Name , String player2Name) {
-        createPlayerIfNotExists(player1Name);
-        createPlayerIfNotExists(player2Name);
-        log.debug("Players created successfully in service");
+        try {
+            createPlayerIfNotExists(player1Name);
+            createPlayerIfNotExists(player2Name);
+            log.debug("Players created successfully in service");
+        }catch (PlayerPersistenceException exception){
+            throw new PlayerServiceException("Error while creating players: " + player1Name + " and " + player2Name);
+        }
     }
 
     private void createPlayerIfNotExists(String playerName) {
