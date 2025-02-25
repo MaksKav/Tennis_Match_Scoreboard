27ec932a4dc45ce3f1517cf626e6f7ca68a8ebd3
Max <kavun.onficial@gmail.com>
2025-02-25 13:15:22 +0100

    feat: implemented PlayerServiceException

---
+++ src/main/java/com/maxkavun/exception/PlayerServiceException.java
@@ -0,0 +1,8 @@
+package com.maxkavun.exception;
+
+public class PlayerServiceException extends RuntimeException {
+
+    public PlayerServiceException(String message) {
+        super(message);
+    }
+}
