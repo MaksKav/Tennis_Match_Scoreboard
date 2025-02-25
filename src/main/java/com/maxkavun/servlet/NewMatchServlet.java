9842f2bda5b3f16ccd16d94a62a7f3c7a08fa3fb
Max <kavun.onficial@gmail.com>
2025-02-25 15:50:41 +0100

    feat: added validation in servlet and added redirect to /match-score

--- src/main/java/com/maxkavun/servlet/NewMatchServlet.java
+++ src/main/java/com/maxkavun/servlet/NewMatchServlet.java
@@ -1,6 +1,11 @@
 package com.maxkavun.servlet;
 
-import com.maxkavun.service.NewMatchService;
+import com.maxkavun.exception.MatchNotFoundException;
+import com.maxkavun.exception.PlayerServiceException;
+import com.maxkavun.service.NewPlayersService;
+import com.maxkavun.service.OngoingMatchesService;
+import com.maxkavun.util.ResponseUtil;
+import com.maxkavun.validator.PlayerNameValidator;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -9,29 +14,56 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
+import java.util.UUID;
 
 
 @Slf4j
 @WebServlet("/new-match")
 public class NewMatchServlet extends HttpServlet {
-    private final NewMatchService newMatchService = new NewMatchService();
+    private final NewPlayersService newMatchService = new NewPlayersService();
+    private final OngoingMatchesService ongoingMatchesService = new OngoingMatchesService();
+    private final PlayerNameValidator playerNameValidator = new PlayerNameValidator();
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
-        response.setCharacterEncoding("UTF-8");
         request.getRequestDispatcher("/new-match.html").forward(request, response);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //TODO вынести назначение контент типа в фильтр
-        response.setContentType("text/html");
-        response.setCharacterEncoding("UTF-8");
         String player1Name = request.getParameter("player1");
         String player2Name = request.getParameter("player2");
-        //TODO добавить валидацию имен
-        newMatchService.createPlayersIfNotExists(player1Name , player2Name);
 
+        if (playerNameValidator.isInvalid(player1Name)) {
+            log.error("Invalid player1 name: {}", player1Name);
+            ResponseUtil.sendResponse(response, HttpServletResponse.SC_BAD_REQUEST, "Invalid player1 name");
+            return;
+        }
 
+        if (playerNameValidator.isInvalid(player2Name)) {
+            log.error("Invalid player2 name: {}", player2Name);
+            ResponseUtil.sendResponse(response, HttpServletResponse.SC_BAD_REQUEST, "Invalid player2 name");
+            return;
+        }
+
+        if (playerNameValidator.areNamesDifferent(player1Name, player2Name)) {
+            log.error("Player 1 and Player 2 are same");
+            ResponseUtil.sendResponse(response, HttpServletResponse.SC_BAD_REQUEST, "Players names must be different");
+            return;
+        }
+        try {
+            newMatchService.createPlayersIfNotExists(player1Name, player2Name);
+            ongoingMatchesService.createMatch(player1Name, player2Name);
+
+            UUID matchId = ongoingMatchesService.getMatchIdByPlayers(player1Name, player2Name);
+            String redirectUrl = "/match-score?uuid=" + matchId.toString();
+            response.sendRedirect(redirectUrl);
+
+        } catch (PlayerServiceException exception) {
+            log.error("Error processing new match request", exception);
+            ResponseUtil.sendResponse(response, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "An error occurred while processing the match");
+        } catch (MatchNotFoundException exception){
+            log.error("Match for players {} and {} not found" , player1Name, player2Name);
+            ResponseUtil.sendResponse(response, HttpServletResponse.SC_NOT_FOUND, "Match for players " + player1Name + " and " + player2Name + " not found");
+        }
     }
 }
