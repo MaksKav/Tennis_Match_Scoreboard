9b81c0bc25c6f63e4df513e926dbe998bf14dd0f
Max <kavun.onficial@gmail.com>
2025-02-25 15:28:23 +0100

    feat: add @Builder for easier object creation, remove matchId initialization

--- src/main/java/com/maxkavun/model/MatchModel.java
+++ src/main/java/com/maxkavun/model/MatchModel.java
@@ -1,14 +1,16 @@
 package com.maxkavun.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import java.util.UUID;
 
+@Builder
 @Getter
 @AllArgsConstructor
 public class MatchModel {
-    private final UUID matchId =  UUID.randomUUID();
-    private final int player1Id;
-    private final int player2Id;
+    private final UUID matchId ;
+    private final Long player1Id;
+    private final Long player2Id;
     private final MatchScoreModel score = new MatchScoreModel();
 }
