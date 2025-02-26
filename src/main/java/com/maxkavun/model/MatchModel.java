7690297d2b8fc4a6b21bdf798d190f4e63e7f5b2
Max <kavun.onficial@gmail.com>
2025-02-26 18:51:39 +0100

    feat: added lombok annotation @ToString and players name

--- src/main/java/com/maxkavun/model/MatchModel.java
+++ src/main/java/com/maxkavun/model/MatchModel.java
@@ -3,13 +3,18 @@ package com.maxkavun.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
+
 import java.util.UUID;
 
+@ToString
 @Builder
 @Getter
 @AllArgsConstructor
 public class MatchModel {
     private final UUID matchId ;
+    private final String player1Name ;
+    private final String player2Name ;
     private final Long player1Id;
     private final Long player2Id;
     private final MatchScoreModel score = new MatchScoreModel();
