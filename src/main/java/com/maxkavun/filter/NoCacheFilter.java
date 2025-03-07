bccc7acfc9c56000584959815d14042428cef85e
Max <kavun.onficial@gmail.com>
2025-03-07 15:27:42 +0100

    feat: implement NoCacheFilter to prevent caching on the client side

---
+++ src/main/java/com/maxkavun/filter/NoCacheFilter.java
@@ -0,0 +1,24 @@
+package com.maxkavun.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter("/*")
+public class NoCacheFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) resp;
+
+        response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        response.setHeader("Pragma", "no-cache");
+        response.setHeader("Expires", "0");
+
+        chain.doFilter(request, response);
+    }
+}
