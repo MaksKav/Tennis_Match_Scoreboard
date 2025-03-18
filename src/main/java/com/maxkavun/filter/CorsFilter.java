4cce1f0ee378e8d1d6aa78f25b615033243bf65d
Max <kavun.onficial@gmail.com>
2025-03-18 15:12:28 +0100

    featL implemented CORS filter

---
+++ src/main/java/com/maxkavun/filter/CorsFilter.java
@@ -0,0 +1,22 @@
+package com.maxkavun.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter("/*")
+public class CorsFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        httpResponse.addHeader("Access-Control-Allow-Origin", "*");
+        httpResponse.addHeader("Access-Control-Allow-Methods", "POST, GET");
+        httpResponse.addHeader("Access-Control-Allow-Headers", "Content-Type");
+
+        chain.doFilter(request, response);
+    }
+}
