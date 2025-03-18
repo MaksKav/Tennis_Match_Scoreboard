15e5a9ee29eb5767c9f235629acf9da073466f45
Max <kavun.onficial@gmail.com>
2025-03-18 15:14:58 +0100

    refactor: renamed from NewPlayersService to NewPlayerService

--- src/main/java/com/maxkavun/service/NewPlayerService.java
+++ src/main/java/com/maxkavun/service/NewPlayerService.java
@@ -9,7 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @NoArgsConstructor
-public class NewPlayersService {
+public class NewPlayerService {
     private final PlayerDao playerDao = new PlayerDao();
 
     public void createPlayersIfNotExists(String player1Name , String player2Name) {
