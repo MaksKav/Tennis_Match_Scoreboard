96b135c2f7b56907ea63c0b9f7477f43853ea9b1
Max <kavun.onficial@gmail.com>
2025-03-19 13:22:12 +0100

    feat: implemented FinishedMatchServiceException

---
+++ src/main/java/com/maxkavun/exception/FinishedMatchServiceException.java
@@ -0,0 +1,8 @@
+package com.maxkavun.exception;
+
+public class FinishedMatchServiceException extends RuntimeException {
+
+    public FinishedMatchServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
