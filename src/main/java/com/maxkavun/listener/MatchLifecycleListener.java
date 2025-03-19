5191071f6ed32a388f53a5b6094a9d93b9b31b04
Max <kavun.onficial@gmail.com>
2025-03-19 13:56:41 +0100

    feat: implemented MatchMonitor and MatchLifecycleListener to remove long-running matches

---
+++ src/main/java/com/maxkavun/listener/MatchLifecycleListener.java
@@ -0,0 +1,31 @@
+package com.maxkavun.listener;
+
+import com.maxkavun.service.MatchMonitor;
+import com.maxkavun.service.OngoingMatchService;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@WebListener
+public class MatchLifecycleListener implements ServletContextListener {
+
+    private OngoingMatchService matchService;
+    private MatchMonitor matchMonitor;
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        matchService = OngoingMatchService.getInstance();
+        matchMonitor = new MatchMonitor(matchService);
+
+        matchMonitor.startMonitoring();
+        log.debug("Match Monitor started");
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        matchMonitor.stopMonitoring();
+        log.debug("Match Monitor stopped");
+    }
+}
---
+++ src/main/java/com/maxkavun/service/MatchMonitor.java
@@ -0,0 +1,37 @@
+package com.maxkavun.service;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.UUID;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.ScheduledExecutorService;
+
+@Slf4j
+public class MatchMonitor {
+    private static final long CHECK_INTERVAL = 60 * 1000; // 1 min
+    private final OngoingMatchService ongoingMatchService;
+    private final ScheduledExecutorService scheduler;
+
+    public MatchMonitor(OngoingMatchService ongoingMatchService) {
+        this.ongoingMatchService = ongoingMatchService;
+        this.scheduler = Executors.newSingleThreadScheduledExecutor();
+    }
+
+    public void startMonitoring() {
+        log.debug("Starting Match Monitor");
+        scheduler.scheduleAtFixedRate(this::checkMatches, 0, CHECK_INTERVAL, TimeUnit.MILLISECONDS);
+    }
+
+    private void checkMatches() {
+        log.debug("Checking Matches from Match Monitor");
+        for (UUID matchId : OngoingMatchService.getOngoingMatches().keySet()) {
+            ongoingMatchService.checkAndResetMatchIfNeeded(matchId);
+        }
+    }
+
+    public void stopMonitoring() {
+        log.debug("Stopping Match Monitor");
+        scheduler.shutdown();
+    }
+}
