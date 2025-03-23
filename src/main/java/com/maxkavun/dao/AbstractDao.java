22ef42af1d967e3a37b5c283d15d3b7b87eb23a5
Max <kavun.onficial@gmail.com>
2025-03-23 13:10:39 +0100

    refactor: remove unusable imports

--- src/main/java/com/maxkavun/dao/AbstractDao.java
+++ src/main/java/com/maxkavun/dao/AbstractDao.java
@@ -7,9 +7,6 @@ import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
-import java.util.List;
-import java.util.Optional;
-
 @Slf4j
 public abstract class AbstractDao<T , E extends AbstractDaoException> implements SavableDao<T> {
     private final SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
